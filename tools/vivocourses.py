4ffe8fe0b918dcc6fda5e1a07abdf5dcf7f197c1
Michael Conlon <mconlon@ufl.edu>
2014-08-23 16:38:43 -0400

    Add test scripts for vivocourses

--- tools/vivocourses.py
+++ tools/vivocourses.py
@@ -37,6 +37,10 @@ def make_course_rdf(taught_data):
     Given taught_data, generate the RDF for a course,
     a teacher role and links between course, teacher role and instructor
     """
+    import tempita
+    from vivofoundation import get_vivo_uri
+    from datetime import datetime
+
     course_rdf_template = tempita.Template("""
     <rdf:Description rdf:about="{{course_uri}}">
         <rdf:type rdf:resource="http://www.w3.org/2002/07/owl#Thing"/>
@@ -47,11 +51,11 @@ def make_course_rdf(taught_data):
         <ufVivo:harvestedBy>Python Courses version 0.5</ufVivo:harvestedBy>
         <ufVivo:dateHarvested>{{harvest_datetime}}</ufVivo:dateHarvested>
     </rdf:Description>""")
-    course_uri = vt.get_vivo_uri()
+    course_uri = get_vivo_uri()
     rdf = course_rdf_template.substitute(course_uri=course_uri,
         course_name=taught_data['course_name'],
         course_number=taught_data['course_number'],
-        harvest_datetime=vt.make_harvest_datetime(),
+        harvest_datetime=datetime.now().isoformat(),
         person_uri=taught_data['person_uri'])
     return [rdf, course_uri]
 
@@ -61,6 +65,9 @@ def make_section_rdf(taught_data):
     the section to its teaching role, to its course and term.  Link the
     role to the instructor.
     """
+    from vivofoundation import get_vivo_uri
+    import tempita
+    from datetime import datetime
     section_rdf_template = tempita.Template("""
     <rdf:Description rdf:about="{{section_uri}}">
         <rdf:type rdf:resource="http://www.w3.org/2002/07/owl#Thing"/>
@@ -92,7 +99,7 @@ def make_section_rdf(taught_data):
         <vivo:roleRealizedIn rdf:resource="{{section_uri}}"/>
     </rdf:Description>""")
 
-    section_uri = vt.get_vivo_uri()
+    section_uri = get_vivo_uri()
     rdf = section_rdf_template.substitute(section_uri=section_uri,
         section_name=taught_data['section_name'],
         section_number=taught_data['section_number'],
@@ -100,10 +107,10 @@ def make_section_rdf(taught_data):
         course_uri=taught_data['course_uri'],
         course_name=taught_data['course_name'],
         course_new=taught_data['course_new'],
-        teacher_role_uri=vt.get_vivo_uri(),
-        course_role_uri=vt.get_vivo_uri(),     
+        teacher_role_uri=get_vivo_uri(),
+        course_role_uri=get_vivo_uri(),
         person_uri=taught_data['person_uri'],
-        harvest_datetime=vt.make_harvest_datetime())
+        harvest_datetime=datetime.now().isoformat())
     return [rdf, section_uri]
 
 def make_taught_dictionary(filename="course_data.csv", debug=False):
@@ -142,32 +149,34 @@ def term_name(term_number):
     """
     Given a UF term number, return the UF term name
     """
-    year = term_number[0:4]
-    term = term_number[4:5]
-    if term == "1":
-        term_name = "Spring "+str(year)
-    elif term == "5" or term == "6" or term == "7":
-        term_name = "Summer "+str(year)
-    elif term == "8":
-        term_name = "Fall "+str(year)
+    if term_number is not None and len(term_number) < 5:
+        year = term_number[0:4]
+        term = term_number[4:5]
+        if term == "1":
+            term_name = "Spring "+str(year)
+        elif term == "5" or term == "6" or term == "7":
+            term_name = "Summer "+str(year)
+        elif term == "8":
+            term_name = "Fall "+str(year)
+        else:
+            raise NoSuchAcademicTermException(term_number)
+        return term_name
     else:
-        print term_number, year, term
         raise NoSuchAcademicTermException(term_number)
-    return term_name
 
 def make_term_dictionary(debug=False):
     """
     Make a term dictionary for academic terms.  Key is term name such as
     "Spring 2011".  Value is URI.
     """
-    query = tempita.Template("""
+    from vivofoundation import vivo_sparql_query
+    query = """
     SELECT ?x ?label
     WHERE {
       ?x a vivo:AcademicTerm .
       ?x rdfs:label ?label .
-    }""")
-    query = query.substitute()
-    result = vt.vivo_sparql_query(query)
+    }"""
+    result = vivo_sparql_query(query)
     try:
         count = len(result["results"]["bindings"])
     except:
